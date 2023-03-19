4c293d32fcaffaa19c48964f9ffcedad888d75d7
andreeatalpos <andreeatalpos2000@yahoo.com>
2023-03-19 12:53:57 +0200

    first commit

---
+++ src/main/java/com/example/LostAnimalsApp/LostAnimalsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.LostAnimalsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAnimalsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostAnimalsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LostAnimalsApp/LostAnimalsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LostAnimalsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostAnimalsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
